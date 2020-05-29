052c86d800fe2e43c3314d6ce4bc39c1cb7a97b9
Kamontat Chantrachirathumrong <kamontat.chantrachirathumrong@agoda.com>
2020-05-29 19:40:43 +0700

    fix(lint): update code

--- internals/config/src/index.ts
+++ internals/config/src/index.ts
@@ -1,6 +1,5 @@
 import jest from "./configs/jest";
 import webpack from "./configs/webpack";
 import eslint from "./configs/eslint";
-import prettier from "./configs/prettier";
 
-export { jest, webpack, eslint, prettier };
+export { jest, webpack, eslint };
