9a21cc8392d5b53ed45318229d86500669b3e28c
Kamontat Chantrachirathumrong <kamontat.chantrachirathumrong@agoda.com>
2020-05-30 01:06:19 +0700

    fix(dev): compiler on react code

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,14 +22,6 @@ module.exports = {
   ],
   parserOptions: {
     tsconfigRootDir: __dirname,
-    ecmaFeatures: {
-      jsx: true, // Allows for the parsing of JSX
-    },
-  },
-  settings: {
-    react: {
-      version: "detect", // Tells eslint-plugin-react to automatically detect the version of React to use
-    },
   },
   rules: {
     "no-tabs": [
