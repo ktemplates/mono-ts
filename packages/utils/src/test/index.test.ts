9365d996d0d3f43db21a4379528d445044c17411
Kamontat Chantrachirathumrong <kamontat.chantrachirathumrong@agoda.com>
2020-06-03 19:13:20 +0700

    perf(test): add new index class test

--- packages/utils/src/test/index.test.ts
+++ packages/utils/src/test/index.test.ts
@@ -1,7 +1,14 @@
+import { Index } from "..";
+
 describe("test", () => {
   test("create new test", () => {
     const num = 1 + 2;
 
     expect(num).toEqual(3);
   });
+
+  test("index class", () => {
+    const index = new Index();
+    expect(index).not.toBeUndefined()
+  })
 });
