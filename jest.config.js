44c72cbfe916f476066afd3bd4fa110dca10e041
Kamontat Chantrachirathumrong <kamontat.chantrachirathumrong@agoda.com>
2020-06-25 16:38:25 +0700

    fix(config): update configuration to new version to resolve jest error

--- jest.config.js
+++ jest.config.js
@@ -8,16 +8,20 @@
 
 const { jest } = require("@kcinternal/configuration");
 const config = jest(__dirname, { root: true }).build();
+
+console.log(config);
 module.exports = config;
 
-// const {jest} = require('@kcinternal/configuration')
-// module.exports = jest(__dirname).build()
-// // for ci to run test
+// for ci to run test
 // module.exports = {
 //   verbose: true,
 //   preset: "ts-jest",
 //   testEnvironment: "node",
-//   reporters: ["default", "jest-junit"packages/**/*.{ts,tsx}", "!internals/**/*.{ts,tsx}", "!typings/**/*.{ts,tsx}"nals/**/*.{ts,tsx}",
+//   reporters: ["default", "jest-junit"],
+//   collectCoverage: true,
+//   collectCoverageFrom: [
+//     "packages/**/*.{ts,tsx}",
+//     "!internals/**/*.{ts,tsx}",
 //     "!typings/**/*.{ts,tsx}",
 //   ],
 //   coveragePathIgnorePatterns: ["/node_modules/", "/lib/"],
