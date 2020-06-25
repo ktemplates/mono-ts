753529d81c23b0f9103c744e8669b31b7c48df34
Kamontat Chantrachirathumrong <kamontat.chantrachirathumrong@agoda.com>
2020-06-25 16:51:07 +0700

    chore: clean code

--- jest.config.js
+++ jest.config.js
@@ -8,22 +8,4 @@
 
 const { jest } = require("@kcinternal/configuration");
 const config = jest(__dirname, { root: true }).build();
-
-console.log(config);
 module.exports = config;
-
-// for ci to run test
-// module.exports = {
-//   verbose: true,
-//   preset: "ts-jest",
-//   testEnvironment: "node",
-//   reporters: ["default", "jest-junit"],
-//   collectCoverage: true,
-//   collectCoverageFrom: [
-//     "packages/**/*.{ts,tsx}",
-//     "!internals/**/*.{ts,tsx}",
-//     "!typings/**/*.{ts,tsx}",
-//   ],
-//   coveragePathIgnorePatterns: ["/node_modules/", "/lib/"],
-//   coverageReporters: ["json", "lcov", "text", "clover"],
-// };
